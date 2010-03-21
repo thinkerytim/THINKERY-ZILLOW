daa268f98d3f4b5512082bf6f8fd14b1be3c557f
Tim Kramer <tim@thethinkery.net>
2010-03-20 17:34:07 -0700

    initial commit

---
+++ mod_ip_zillow/helper.php
@@ -0,0 +1,151 @@
+<?php
+/**
+ * @copyright Copyright (C) 2010 The Thinkery LLC - www.thethinkery.net. All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+ */
+
+defined('_JEXEC') or die('Direct Access to this location is not allowed.');
+ 
+class ModIpZillowHelper
+{
+    
+    // this function grabs the zpid and other data for a given address
+	public function GetSearchResults( $params ){
+		
+		# required params: zwsid, address, citystatezip
+		
+		$query_string = "";
+		
+		$Zparams = array(
+			'zws-id' 		=> $params['zws-id'],
+			'address' 		=> $params['address'],
+			'citystatezip' 	=> $params['citystatezip']
+		);
+		
+		foreach ($Zparams as $key => $value) {
+			$query_string .= "$key=" . urlencode($value) . "&";
+		}		
+		
+		$url = "http://www.zillow.com/webservice/GetSearchResults.htm?$query_string";	
+		
+		$output = file_get_contents($url);
+		$output = new SimpleXMLElement($output);
+		return $output;
+	}
+	
+	// this function grabs the chart for a zpid 
+	public function GetChart( $params ){
+		
+		# required params: zwsid, zpid, unit_type, width, height, duration
+		
+		$query_string = "";
+		
+		$Zparams = array(
+			'zws-id' 		=> $params['zws-id'],
+			'zpid' 			=> $params['zpid'],
+			'unit-type' 	=> $params['unit_type'],
+			'width' 		=> $params['width'],
+			'height'		=> $params['height'],
+			'chartDuration' => $params['duration']
+		);
+		
+		foreach ($Zparams as $key => $value) {
+			$query_string .= "$key=" . urlencode($value) . "&";
+		}		
+		
+		$url = "http://www.zillow.com/webservice/GetChart.htm?$query_string";
+		
+		$output = file_get_contents($url);
+		$output = new SimpleXMLElement($output);
+		if ($output->message[0]->code != 0){
+			return "There was a problem with the Zillow Chart request-- Error code " . $output->message[0]->code . "<br />" . $url;
+		}else{
+			return $output->response[0]->url;
+		}
+	}	
+	
+	// this function grabs the region chart
+	public function GetRegionChart( $params ){
+		
+		# required params: zwsid, zpid, unit_type, width, height, duration, city, state, zip, type
+		
+		$query_string = "";
+		
+		$Zparams = array(
+			'zws-id'		=> $params['zws-id'],
+			'unit-type' 	=> $params['unit_type'],
+			'width'			=> $params['width'],
+			'height'		=> $params['height'],
+			'chartDuration' => $params['duration']
+		);
+		
+		
+		if ($params['type'] == 'city'){
+			$Zparams['city'] = $params['city'];
+			$Zparams['state'] = $params['state'];
+		} elseif ($params['type'] == 'zip') {
+			$Zparams['zip'] = $params['zip'];
+		}
+							
+		foreach ($Zparams as $key => $value) {
+			$query_string .= "$key=" . urlencode($value) . "&";
+		}		
+		
+		$url = "http://www.zillow.com/webservice/GetRegionChart.htm?$query_string";
+		
+		$output = file_get_contents($url);
+		$output = new SimpleXMLElement($output);
+		if ($output->message[0]->code != 0){
+			return "There was a problem with the Zillow Chart request-- Error code " . $output->message[0]->code . "<br />" . $url;
+		}else{
+			return $output->response[0]->url;
+		}
+	}	
+	
+	// this function grabs the comps a zpid
+	public function GetComps( $params ){
+		
+		# required params: zwsid, zpid, count
+		
+		$query_string = "";
+		$i = 0;		
+		$count = $params['count'];
+		
+		$Zparams = array(
+			'zws-id' 	=> $params['zws-id'],
+			'zpid' 		=> $params['zpid'],
+			'count' 	=> $params['count']
+		);
+		
+		foreach ($Zparams as $key => $value) {
+			$query_string .= "$key=" . urlencode($value) . "&";
+		}		
+		
+		$url = "http://www.zillow.com/webservice/GetComps.htm?$query_string";	
+		
+		$output = file_get_contents($url);
+		$output = new SimpleXMLElement($output);
+		if ($output->message[0]->code != 0){
+			return "There was a problem with the Zillow Comps request-- Error code " . $output->message[0]->code . "<br />" . $url;
+		}else{	
+			
+			$html = "<div class=" . $init_params['class'] . "><table>";
+			$html .= "<thead><tr><td><b>Address:</b></td><td>&nbsp;</td><td><b>Valuation:</b></td></tr></thead>";
+			$html .= "<tbody>";
+			while ( $i < $count ) {
+				$html .= "<tr><td><a href=\"" . $output->response->properties->comparables->comp[$i]->links->homedetails . "\" target=\"_blank\">" . $output->response->properties->comparables->comp[$i]->address[0]->street . ", " . $output->response->properties->comparables->comp[$i]->address[0]->city;
+				$html .= ", " . $output->response->properties->comparables->comp[$i]->address[0]->state;
+				$html .= ", " . $output->response->properties->comparables->comp[$i]->address[0]->zipcode;
+				$html .= "</a></td><td>&nbsp;</td>";
+				$html .= "<td>$" . number_format($output->response->properties->comparables->comp[$i]->zestimate->amount) . "</td></tr>";
+				 
+				$i++;
+			}			
+			$html .= "</tbody></table></div>";		
+			
+			return $html;
+		}
+	}
+ 
+} //end ModIpZillowHelper
+?>
---
+++ mod_ip_zillow/mod_ip_zillow.php
@@ -0,0 +1,177 @@
+<?php
+/**
+ * @copyright Copyright (C) 2010 The Thinkery LLC - www.thethinkery.net. All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+ */
+
+defined('_JEXEC') or die('Direct Access to this location is not allowed.');
+ 
+require_once(dirname(__FILE__).DS.'helper.php');
+jimport( 'joomla.html.pane' );
+if(!$params->get('zws-id')) {
+	echo "Please provide a valid Zillow API ID";
+	return;
+}
+
+if (JRequest::getVar('address')) {
+	$address 		= JRequest::getVar('address');
+	$citystatezip 	= JRequest::getVar('citystatezip');
+	$render_tabs = true;
+}
+
+$this->baseurl = JURI::root(true);
+$document 	   = & JFactory::getDocument();
+$document->addStyleSheet($this->baseurl.'/modules/mod_ip_zillow/tmpl/ip_zillow.css');
+
+
+$prop_info = false;
+
+// get params
+$init_params = array(
+		'title'				=> $params->get('tabtitle'),
+		'zws-id' 			=> $params->get('zws-id'),
+		'width' 			=> $params->get('width'),
+		'height'			=> $params->get('height'),
+		'duration'			=> $params->get('chartDuration'),
+		'unit_type' 		=> $params->get('unit-type'),
+		'count' 			=> $params->get('count'),
+		'show_info' 		=> $params->get('show_info'),
+		'show_chart' 		=> $params->get('show_chart'),
+		'show_region_chart' => $params->get('show_region_chart'),
+		'show_comps' 		=> $params->get('show_comps'),
+		'class'				=> $params->get('MODULECLASSSUFFIX')
+		);
+
+$zillow =& JPane::getInstance('tabs', array('startOffset'=>0));
+echo $zillow->startPane( 'pane' );
+echo $zillow->startPanel( "Input Address", 'zpanel1' );
+	echo "<div class=" . $init_params['class'] . ">
+			<table>
+				<form name=\"zillow\" action=\"" . JFactory::getURI()->_uri . "\" method=\"post\">	
+				<tr>
+					<td><b>Address: </b></td><td>&nbsp;</td><td><input type=\"text\" name=\"address\" value=\"" . $address . "\"></td>
+				</tr>
+				<tr>								
+					<td><b>City, State OR Zip: </b></td><td>&nbsp;</td><td><input type=\"text\" name=\"citystatezip\" value=\"" . $citystatezip . "\"></td>
+				</tr>
+				<tr>	
+					<td><input type=\"Submit\" value=\"Submit\"></td><td>&nbsp;</td>
+				</tr>
+				</form>
+		 </table>
+		 </div>";
+echo $zillow->endPanel();
+
+if ($render_tabs) {
+	$zwsid			= $params->get('zws-id'); 
+	
+	$info_params = array(
+		'zws-id' 		=> $zwsid,
+		'address' 		=> $address,
+		'citystatezip' 	=> $citystatezip
+		);
+	
+		$output = ModIpZillowHelper::GetSearchResults($info_params);
+		if ($output->message[0]->code != 0){
+			echo "There was a problem with the Zillow request-- Error code " . $output->message[0]->code . "<br /> Please check your address and try again.";
+			return;
+		}else{
+		
+			if ($init_params['show_info']){
+				echo $zillow->startPanel( $init_params['title'], 'zpanel2' );
+					if ($output) {
+						
+						$zpid	 		=	$output->response[0]->results[0]->result[0]->zpid;
+						$neighborhood 	= 	$output->response[0]->results[0]->result[0]->localRealEstate[0]->region['name'];
+						$city 			=	$output->response[0]->results[0]->result[0]->address[0]->city;
+						$state 			=	$output->response[0]->results[0]->result[0]->address[0]->state;
+						$zip 			=	$output->response[0]->results[0]->result[0]->address[0]->zipcode;
+						
+						echo "<div class=\"ip_zillowmod\">";
+						echo "<table>";
+						echo "<tr><td><b>Zestimate:</b></td><td>&nbsp;</td><td> $" . number_format($output->response[0]->results[0]->result[0]->zestimate[0]->amount) . "</td></tr>";
+						echo "<tr><td><b>Changed:</b></td><td>&nbsp;</td><td> $" . number_format($output->response[0]->results[0]->result[0]->zestimate[0]->valueChange) . " in last " .  $output->response[0]->results[0]->result[0]->zestimate[0]->valueChange['duration'] . " days</td></tr>";
+						echo "<tr><td><b>Last updated:</b></td><td>&nbsp;</td><td> " . $output->response[0]->results[0]->result[0]->zestimate[0]->{'last-updated'} . "</td></tr>";
+						echo "<tr><td><b>Low value over period:</b></td><td>&nbsp;</td><td> $" . number_format($output->response[0]->results[0]->result[0]->zestimate[0]->valuationRange->low) . "</td></tr>";
+						echo "<tr><td><b>High value over period:</b></td><td>&nbsp;</td><td> $" . number_format($output->response[0]->results[0]->result[0]->zestimate[0]->valuationRange->high) . "</td></tr>";
+						echo "</table>";
+						echo "</div>";
+					} else {
+						echo "No Chart found";
+					}
+				echo $zillow->endPanel();
+			}
+			
+			if ($init_params['show_chart']){
+				# zwsid, zpid, unit_type, width, duration
+				$chart_params = array(
+								'zws-id' 		=> $zwsid,
+								'zpid' 			=> $zpid,
+								'unit_type' 	=> $init_params['unit_type'],
+								'width'			=> $init_params['width'],
+								'height'			=> $init_params['height'],
+								'duration'		=> $init_params['duration']
+								);
+				
+				$chart = ModIpZillowHelper::GetChart( $chart_params );
+				echo $zillow->startPanel( 'Zillow Property Value Chart', 'zpanel3' );
+					if ($chart) {
+						echo "<div class=\"ip_zillowmod\"><img src=\"" . $chart . "\"><br /><br />See more graphs and data about $address on Zillow.com.</div>";
+					} else {
+						echo "No Chart found";
+					}
+				echo $zillow->endPanel();
+			}
+			
+			if ($init_params['show_region_chart']){
+				# zwsid, zpid, unit_type, width, duration, city, state, zip, type
+				$region_params = array(
+								'zws-id' 		=> $zwsid,
+								'zpid' 			=> $zpid,
+								'unit_type' 	=> $init_params['unit_type'],
+								'width'			=> $init_params['width'],
+								'height'		=> $init_params['height'],
+								'duration'		=> $init_params['duration'],
+								'city'			=> $city,
+								'state'			=> $state,
+								'type'			=> 'city'
+								);				
+				
+				$r_chart = ModIpZillowHelper::GetRegionChart( $region_params );
+				echo $zillow->startPanel( 'Zillow Region Value Chart', 'zpanel4' );
+					if ($r_chart) {
+						echo "<div class=\"ip_zillowmod\"><img src=\"" . $r_chart . "\"><br /><br />See more graphs and data about $address on Zillow.com.</div>";
+					} else {
+						echo "No Chart found";
+					}
+				echo $zillow->endPanel();
+			}
+			
+			if ($init_params['show_comps']){
+				# zwsid, zpid, count
+				$comp_params = array(
+								'zws-id' 		=> $zwsid,
+								'zpid' 			=> $zpid,
+								'count' 		=> $init_params['count']
+								);
+				
+				
+				$comps = ModIpZillowHelper::GetComps( $comp_params );
+				echo $zillow->startPanel( 'Zillow Comps', 'zpanel5' );
+					echo $comps;
+				echo $zillow->endPanel();
+			}
+	}		
+} 
+
+		echo $zillow->endPane();
+		echo "<br /><br /><a href=\"http://zillow.com\" target=\"_blank\"><img src=\"http://www.zillow.com/widgets/GetVersionedResource.htm?path=/static/logos/Zillowlogo_150x40.gif\" width=\"150\" height=\"40\" alt=\"Zillow Real Estate Search\" /></a>";
+		echo "<br /><br /><a href=\"http://www.thethinkery.net\" target=\"_blank\">Zillow module by The Thinkery</a>";
+		return true;	
+ 
+// include the template for display
+require(JModuleHelper::getLayoutPath('mod_ip_zillow'));
+?>
+
+
+
